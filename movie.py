08ec5882ff3742d54f24df6e183369fd58c63c95
tarothanawat <115664618+tarothanawat@users.noreply.github.com>
2024-10-20 20:31:35 +0700

    Add Attributes to Movie.

--- movie.py
+++ movie.py
@@ -1,11 +1,17 @@
+from dataclasses import dataclass, field
+from typing import Collection
 
+
+@dataclass(frozen=True)
 class Movie:
-    """A movie available for rent."""
-    def __init__(self, title):
-        self.title = title
+    """A movie available for rent. Immutable."""
+    title: str
+    year: int
+    genre: Collection[str] = field(default_factory=list)
 
-    def get_title(self):
-        return self.title
+    def is_genre(self, string: str) -> bool:
+        #  any: check if something exists in a collection.
+        return any(g.lower() == string.lower().strip() for g in self.genre)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
