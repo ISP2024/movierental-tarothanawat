882131c19501c5d0f8aff0d0466f92d448596da0
tarothanawat <115664618+tarothanawat@users.noreply.github.com>
2024-10-20 23:01:08 +0700

    Retype all the tests and in main.

--- pricing_test.py
+++ pricing_test.py
@@ -8,7 +8,7 @@ class PricingTest(unittest.TestCase):
     def setUp(self):
         self.new_movie = Movie("Dune: Part Two", 2024, ['Children', 'Action'])
         self.regular_movie = Movie("Air", 2000, 'Adult')
-        self.children_movie = Movie("Frozen", 2018,['Children', 'Comedy'])
+        self.children_movie = Movie("Frozen", 2018, ['Children', 'Comedy'])
         self.childrens_movie = Movie("For Childrens", 2018, ['Childrens', 'Comedy'])
 
     def test_get_pricing_strategy(self):
@@ -17,3 +17,6 @@ class PricingTest(unittest.TestCase):
         self.assertIsInstance(price_strategy_for_movie(self.childrens_movie), ChildrensPrice)
         self.assertIsInstance(price_strategy_for_movie(self.children_movie), ChildrensPrice)
 
+
+if __name__ == '__main__':
+    unittest.main()
